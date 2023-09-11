fe833587d419bec0f401f23a66bece2ae96c1e7f
Dylan Danowski <dylandanowski@gmail.com>
2023-09-11 10:36:53 -0400

    Add project files.

---
+++ SNGHardcoreWoWBot/Commands/BaseCommands.cs
@@ -0,0 +1,33 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNGHardcoreWoWBot.Commands
+{
+    public class BaseCommands : BaseCommandModule
+    {
+        [Command("register")]
+        public async Task RegisterPlayerCommand(CommandContext commandContext)
+        {
+            await commandContext.RespondAsync($"Registered {commandContext.User.Username}");
+        }
+
+        [Command("addcharacter")]
+        public async Task RegisterPlayerCharacterCommand(CommandContext commandContext, string characterName)
+        {
+            await commandContext.RespondAsync($"{characterName} added for {commandContext.User.Username}");
+        }
+
+        [Command("characterstatus")]
+        public async Task ChangeCharacterStatus(CommandContext commandContext, string characterName, string status)
+        {
+            await commandContext.RespondAsync($"{characterName} {status}");
+        }
+    }
+}
---
+++ SNGHardcoreWoWBot/Program.cs
@@ -0,0 +1,32 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics.Metrics;
+using SNGHardcoreWoWBot.Commands;
+
+namespace SNGHardcoreWoWBot
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "MTE0OTkyODIwNDI5MTgwNTI0Ng.GarktY.Teht5l081cCloh26Fc4EATsB4Z8H2TOpPf8IMM",
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.All,
+                MinimumLogLevel = LogLevel.Debug
+            });
+
+            var commands = discord.UseCommandsNext(new CommandsNextConfiguration()
+            {
+                StringPrefixes = new[] {"!"}
+            });
+
+            commands.RegisterCommands<BaseCommands>();
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
