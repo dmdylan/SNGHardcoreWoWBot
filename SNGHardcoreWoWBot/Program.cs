d3a62d84d9c9a1a42c24ef218e42c6da1e044b58
dmdylan <51095343+dmdylan@users.noreply.github.com>
2023-09-12 13:49:28 -0400

    Supabase stuff

--- SNGHardcoreWoWBot/Program.cs
+++ SNGHardcoreWoWBot/Program.cs
@@ -1,14 +1,15 @@
 ﻿using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using Microsoft.Extensions.Logging;
-using System.Diagnostics.Metrics;
 using SNGHardcoreWoWBot.Commands;
+using Supabase;
 
 namespace SNGHardcoreWoWBot
 {
     internal class Program
     {
-        static async Task Main(string[] args)
+        //TODO: Move everything to own class?
+        private static async Task Main(string[] args)
         {
             var discord = new DiscordClient(new DiscordConfiguration
             {
@@ -20,12 +21,24 @@ namespace SNGHardcoreWoWBot
 
             var commands = discord.UseCommandsNext(new CommandsNextConfiguration()
             {
-                StringPrefixes = new[] {"!"}
+                StringPrefixes = new[] { "!" }
             });
 
             commands.RegisterCommands<BaseCommands>();
 
+            string url = "https://xsnpijpuuaxcfwkwxjxi.supabase.co";
+            string key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhzbnBpanB1dWF4Y2Z3a3d4anhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ0Nzc0NjAsImV4cCI6MjAxMDA1MzQ2MH0.UdatJdbEs2r6kdHuo5cTkNWDckCxs8uVFjip7_EB99E";
+
+            SupabaseOptions options = new SupabaseOptions
+            {
+                AutoConnectRealtime = true
+            };
+
+            Client supabase = new Client(url, key, options);
+
             await discord.ConnectAsync();
+            await supabase.InitializeAsync();
+
             await Task.Delay(-1);
         }
     }
